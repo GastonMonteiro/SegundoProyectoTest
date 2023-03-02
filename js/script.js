7b3f016e2fb1e8e833eead1095b69087d4291de7
Gaston Monteiro Eichele <monteirogaston@gmail.com>
2023-03-01 23:53:51 -0300

    Commit 1-03

--- js/script.js
+++ js/script.js
@@ -19,9 +19,10 @@ if(document.title==="index") {
             <div class="text-center aling-items-center d-flex justify-content-between">
                 <span class="fw-bold fs-5 mt-1">$ ${precio}</span>
                 <a href="" class="btn btn-warning">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
-                        <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-                    </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+                <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
+                <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+              </svg>
                 </a>
             </div>
           </div>
@@ -50,7 +51,7 @@ if(document.title==="administracion") {
 
 const botonAgregar = document.getElementById("button-addon2")
 const inputNombre = document.getElementById("inputNombre")
-const inputCodigo = document.getElementById("inputCodigo")
+const inputCodigo = Date.now()
 const inputCategoria = document.getElementById("inputCategoria")
 const inputPrecio = document.getElementById("inputPrecio")
 const inputImagen = document.getElementById("inputImagen")
@@ -74,11 +75,10 @@ class producto {
     }
 }
 
-
 botonAgregar.addEventListener("click",(e) => {
     location.reload();
     const nombre = inputNombre.value
-    const codigo = inputCodigo.value
+    const codigo = inputCodigo
     const categoria = inputCategoria.value
     const precio = inputPrecio.value
     const imagen = inputImagen.value
@@ -127,13 +127,12 @@ botonAgregar.addEventListener("click",(e) => {
     htmlTemplate += "</div>";
     
     divApp.innerHTML = htmlTemplate;
-}
+  }
 
 if(document.title==="registro") {
 
-const botonRegistro = document.getElementById("botonRegistro")
-
-const inputNombre = document.getElementById("inputNombre")
+const botonRegistro = document.getElementById("button-addon2")
+const inputNombreU = document.getElementById("inputNombre")
 const inputApellido = document.getElementById("inputApellido")
 const inputEmail = document.getElementById("inputEmail")
 const inputPassword1 = document.getElementById("inputPassword1")
@@ -154,27 +153,56 @@ class Usuario {
 }
 
 botonRegistro.addEventListener("click", (e) => {
-    const nombre = inputNombre.value
+    location.reload();
+    const nombre = inputNombreU.value
     const apellido = inputApellido.value
     const email = inputEmail.value.toLowerCase()
     const password1 = inputPassword1.value
     const password2 = inputPassword2.value
-  
     usuarios.map((usuario) => {
       if (usuario.email === email) {
         alert("El email ya se encuentra registrado.")
         return false
       };
     });
-
     if (password1 != password2) {
-        alert("Las contraseñas no coinciden")
+        alert("Las contraseñas no coinciden.")
         return false
     }
-
-    const nuevoUsuario = new Usuario(nombre,apellido,email,password1)
+    const nuevoUsuario = new Usuario( nombre, apellido, email, password1)
     usuarios.push(nuevoUsuario)
     localStorage.setItem('Usuario', JSON.stringify(usuarios))
+    alert("Gracias por registrarse")
+    location.href ="/index.html";
   })
 
+  const divUsuarios = document.querySelector("#divUusuarios")
+
+    
+  const liTemplate = ( nombre, apellido, email, password1 ) => {
+    return `<ul>
+    <li>${nombre}</li>
+    <li>${apellido}</li>
+    <li>${email}</li>
+    <li>${password1}</li>
+            </ul>`;
+  };
+  
+
+  let ulTemplate = "<ul class='d-flex row mx-2 justify-content-around'>";
+  
+  agregados.map((element) => {
+    const { nombre, apellido, email, password1 } =
+      element;
+    htmlTemplate += liTemplate(
+      nombre,
+      apellido,
+      email,
+      password1,
+    );
+  });
+  
+  ulTemplate += "</div>";
+  
+  divUsuarios.innerHTML = htmlTemplate;
 }
